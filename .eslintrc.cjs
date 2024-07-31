3623e7cff2ef1a959c941aebf654593e7317f66e
xnng <xnng77@gmail.com>
2024-07-31 20:09:50 +0800

    添加 layout 组件

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,6 @@
 // .eslintrc.cjs
 const { defineConfig } = require('eslint-define-config')
+const unocss = require('@unocss/eslint-config/flat')
 
 module.exports = defineConfig({
   root: true,
@@ -23,13 +24,15 @@ module.exports = defineConfig({
     'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    '@vue/eslint-config-prettier'
+    '@vue/eslint-config-prettier',
+    '@unocss'
   ],
   rules: {
     'prettier/prettier': 'error',
     '@typescript-eslint/no-var-requires': 'off', // 允许使用 require
     'vue/multi-word-component-names': 'off', // 允许使用单个单词的组件名
     '@typescript-eslint/no-explicit-any': 'off', // 允许使用 any
+    '@unocss/order-attributify': 'off', // 关闭 unocss 类名必须按一定规则排序
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAppStore } from './store/app'
+
+const appStore = useAppStore()
+appStore.initMobileListener()
+</script>
 
 <template>
   <RouterView />
