d2c4803247d525ebb1cc14a425f75d1972ecf0c9
xnng <xnng77@gmail.com>
2024-07-31 20:22:46 +0800

    修复 lint

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,35 +1,31 @@
 <template>
   <section class="w-[100%] h-[100%] relative">
     <div v-if="layout === sideLayout">
+      <div :class="['absolute top-0 left-0 h-full layout-border__right', { '!fixed z-3000': mobile }]">
+        <div>logo</div>
+        <div>menu</div>
+      </div>
       <div
-          :class="['absolute top-0 left-0 h-full layout-border__right', { '!fixed z-3000': mobile }]">
-          <div>logo</div>
-          <div>menu</div>
-        </div>
-        <div
-          :class="[
-            'absolute top-0 h-[100%]',
-            {
-              'w-[calc(100%-var(--left-menu-min-width))] left-[var(--left-menu-min-width)]':
-                collapse && !mobile && !mobile,
-              'w-[calc(100%-var(--left-menu-max-width))] left-[var(--left-menu-max-width)]':
-                !collapse && !mobile && !mobile,
-              'fixed !w-full !left-0': mobile
-            }
-          ]""
-          style="transition: all var(--transition-time-02);"
-        >
-          <ElScrollbar>
-            <router-view>
+        :class="[
+          'absolute top-0 h-[100%]',
+          {
+            'w-[calc(100%-var(--left-menu-min-width))] left-[var(--left-menu-min-width)]': collapse && !mobile && !mobile,
+            'w-[calc(100%-var(--left-menu-max-width))] left-[var(--left-menu-max-width)]': !collapse && !mobile && !mobile,
+            'fixed !w-full !left-0': mobile
+          }
+        ]"
+        style="transition: all var(--transition-time-02)"
+      >
+        <ElScrollbar>
+          <router-view>
             <template #default="{ Component, route }">
               <!-- <keep-alive :include="getCaches"> -->
               <component :is="Component" :key="route.fullPath" />
               <!-- </keep-alive> -->
             </template>
           </router-view>
-
-          </ElScrollbar>
-        </div>
+        </ElScrollbar>
+      </div>
     </div>
   </section>
 </template>
