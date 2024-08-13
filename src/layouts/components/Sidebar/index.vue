fbc0931880014e3ebc77db10e31be057f974d287
xnng <xnng77@gmail.com>
2024-08-13 14:47:25 +0800

    修改菜单样式

--- src/layouts/components/Sidebar/index.vue
+++ src/layouts/components/Sidebar/index.vue
@@ -41,7 +41,7 @@ const sidebarMenuHoverBgColor = computed(() => {
   return !isTop.value ? 'var(--admin-sidebar-menu-hover-bg-color)' : 'transparent'
 })
 const tipLineWidth = computed(() => {
-  return !isTop.value ? '2px' : '0px'
+  return !isTop.value ? '4px' : '0px'
 })
 // 当为顶部模式时隐藏垂直滚动条
 const hiddenScrollbarVerticalBar = computed(() => {
